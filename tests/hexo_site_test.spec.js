f999763ad84edc387278cd1581fbcd3c8b24ba54
dfianuale <148498363+dfianuale@users.noreply.github.com>
2024-10-03 23:22:42 -0400

    Update hexo_site_test.spec.js

--- tests/hexo_site_test.spec.js
+++ tests/hexo_site_test.spec.js
@@ -2,6 +2,6 @@ const {test, expect} = require('@playwright/test')
 
 test('Test if Hexo site exists and is running', async({page}) => {
 
-    await page.goto('localhost:4000')
+    await page.goto('http://localhost:4000/')
     await expect(page).toHaveTitle('Hexo')
 })
