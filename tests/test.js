84688414b5e061d616cdd4e41422f8a1f4877446
Shaan Malik <99844393+Shaan6695@users.noreply.github.com>
2024-10-18 07:20:21 -0400

    adding some tests

--- tests/test.js
+++ tests/test.js
@@ -1,1 +1,6 @@
+//Playwright Tests for Agile Docs
 
+
+test('Verify homepage link, title, and article heading', async ({ page }) => {
+  // Go to the homepage
+  await page.goto('https://dfianuale.github.io/hexo-IS373/');
