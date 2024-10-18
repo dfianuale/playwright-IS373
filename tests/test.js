41cee0260a8c182ad61e879afcabc51ec5b23339
Shaan Malik <99844393+Shaan6695@users.noreply.github.com>
2024-10-18 07:21:28 -0400

    adding some more tests

--- tests/test.js
+++ tests/test.js
@@ -4,3 +4,9 @@
 test('Verify homepage link, title, and article heading', async ({ page }) => {
   // Go to the homepage
   await page.goto('https://dfianuale.github.io/hexo-IS373/');
+
+// Check the homepage URL
+  await expect(page).toHaveURL('https://dfianuale.github.io/hexo-IS373/');
+
+  // Verify the page title
+  await expect(page).toHaveTitle(/Hexo/); // Assuming "Hexo" is part of the title
